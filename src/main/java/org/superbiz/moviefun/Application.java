14fd6465a998818189777978510f2298f26b1265
Damien Le Berrigaud <damien@itsdamo.com>
2016-12-14 10:52:33 -0700

    Original
    Create SpringBootApplication class
    Map index to index.jsp.
    Map setup page.
    Map ActionServlet, move @Transactional into MoviesBean.
    Cleanup unused configuration files.
    Add albums to the app.
    Basic CRUD implementation for Album Covers.
    Extracted BlobStore interface and FileStore implementation.
    Introduce S3 Store replacing File Store.
    Introduce printing of messages on STDOUT and STDERR.
    Replace message outputs with SLF4J logger usage.
    Setup log levels.
    Import Albums from BlobStore at regular interval.
    Update Albums at regular interval, ensure only one application instance does it.
    Allow people to test the modernization labs locally with Minio

---
+++ src/main/java/org/superbiz/moviefun/Application.java
@@ -0,0 +1,39 @@
+package org.superbiz.moviefun;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3Client;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.superbiz.moviefun.blobstore.BlobStore;
+import org.superbiz.moviefun.blobstore.S3Store;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String... args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public ServletRegistrationBean actionServletRegistration(ActionServlet actionServlet) {
+        return new ServletRegistrationBean(actionServlet, "/moviefun/*");
+    }
+
+    @Value("${s3.endpointUrl}") String s3EndpointUrl;
+    @Value("${s3.accessKey}") String s3AccessKey;
+    @Value("${s3.secretKey}") String s3SecretKey;
+    @Value("${s3.bucketName}") String s3BucketName;
+
+    @Bean
+    public BlobStore blobStore() {
+        AWSCredentials credentials = new BasicAWSCredentials(s3AccessKey, s3SecretKey);
+        AmazonS3Client s3Client = new AmazonS3Client(credentials);
+        s3Client.setEndpoint(s3EndpointUrl);
+
+        return new S3Store(s3Client, s3BucketName);
+    }
+}
